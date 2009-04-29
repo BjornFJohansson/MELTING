dd0d39ac41eb43926eae39a80876d57c9339a29e
marine3 <marine3@390dfc27-8b09-476f-b734-a0a30a918ef6>
2009-04-29 14:28:51 +0000

    the different approximative methods are implemented. I added the formula to have the sodium equivalent concentration if we have magnesium and other monovalent cations

--- trunk/melting-java/src/melting/SodiumEquivalent.java
+++ trunk/melting-java/src/melting/SodiumEquivalent.java
@@ -1,7 +1,10 @@
 package melting;
 
+import java.util.HashMap;
+
 public interface SodiumEquivalent {
 
-	public double getSodiumEquivalent();
+	public double getSodiumEquivalent(double Na, double Mg, double K, double Tris, double dNTP);
+	public boolean isApplicable(HashMap<String, String> options);
 	
 }
