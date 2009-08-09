5bf60ebe7840dca7c84448cb4c16cc8132eeff34
marine3 <marine3@390dfc27-8b09-476f-b734-a0a30a918ef6>
2009-08-09 13:51:48 +0000

    generated the user documentation

---
+++ melting-java/doc/html/labels.pl
@@ -0,0 +1,245 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_SantaLucia_95_1998/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Blake_2_1996/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Wright_46_2007/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Amber_47_2008/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_SantaLucia_33_2004/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Sugimoto_354_1994/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Joshua_46_2007/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Jaya_46_2007/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Broda_44_2005/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bommarito_28_2000/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Nicolas_47_2001/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Record_5_1967/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Freier_83_1986/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Allawi_26_1998/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Richard_43_2004/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Watkins_33_2005/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Elzbieta_34_2006/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Chester_209_1993/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Owen_7_1969/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Breslauer_83_1986/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Wahl_152_1987/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Allawi_37_1998/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Peyret_38_1999/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Zhi_34_2006/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_McConaughy_8_1969/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Zhi_92_2007/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Blake_26_1998/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_R_24_1996/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Fumiaki_43_2004/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hall_16_1980/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Amber_46_2007/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Musielski_21_1981/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Junji_29_2001/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Sugimoto_34_1995/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Peyret_5_2000/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Owczarzy_47_2008/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Tatsuo_124_2002/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_SantaLucia_35_1996/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Wetmur_26_1991/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Allawi_36_1997/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Schildkraut_3_1965/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Amanda_11_2005/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hutton_4_1977/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Zhi_90_2006/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Marmur_5_1962/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Cullen_3_1976/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Frank_10_1971/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Patricia_43_2004/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hiroyuki_49_2005/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mitsuhashi_10_1996/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Escara_19_1980/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Amanda_34_2006/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Casey_4_1977/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Stacy_36_2008/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Britten_29_1974/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_David_288_1999/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Sugimoto_24_1996/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Xia_37_1998/;
+$external_labels{$key} = "$URL/" . q|melting.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
