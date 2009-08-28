b0c723004cee2d3e01cb079a0b0cf178be1adcdf
marine3 <marine3@390dfc27-8b09-476f-b734-a0a30a918ef6>
2009-08-28 09:16:30 +0000

    Added a compatibility between MELTING 4 and MELTING 5 options. (except for the option '-q')

---
+++ melting-java/builder.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+TMPFOLDER=/tmp/meltingbuild
+INNER_ARCHIVE_FOLDER=MELTING5
+ARCHIVE_NAME=MELTING5
+DSTFOLDER=${TMPFOLDER}/${INNER_ARCHIVE_FOLDER}
+echo "Preparing temporary folder ${DSTFOLDER}"
+mkdir -p ${DSTFOLDER}
+
+echo "Copying sources"
+cp -Rf src ${DSTFOLDER}
+
+echo "Copying data"
+cp -Rf Data ${DSTFOLDER}
+
+echo "Copying documentations"
+cp -Rf doc javadoc ${DSTFOLDER}
+
+echo "Copying binaries"
+cp -Rf executable ${DSTFOLDER}
+
+echo "Copying miscellaneous"
+cp -Rf testResults ${DSTFOLDER}
+cp ChangeLog GeneralPublicLicence Install.unices README ${DSTFOLDER}
+
+echo "Adding execution rights"
+chmod a+x ${DSTFOLDER}/executable/*
+chmod a+x ${DSTFOLDER}/Install.unices
+
+cd ${DSTFOLDER}
+find -name .svn -prune -exec rm -Rf '{}' \;
+cd -
+
+echo "Packing into tgz"
+tar -C ${TMPFOLDER} -czf ${ARCHIVE_NAME}.tar.gz ${INNER_ARCHIVE_FOLDER} && echo "\t ${ARCHIVE_NAME}.tar.gz created"
+echo "Packing into zip"
+cd ${TMPFOLDER}
+zip ${ARCHIVE_NAME}.zip ${INNER_ARCHIVE_FOLDER}/ && echo "\t ${ARCHIVE_NAME}.zip done"
+cd -
+
+echo "Cleaning"
+#rm -Rf ${TMPFOLDER}
