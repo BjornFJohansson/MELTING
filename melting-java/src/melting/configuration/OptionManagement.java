ee8777e054b4092c8ae052c33b6c2c5473c7747e
marine3 <marine3@390dfc27-8b09-476f-b734-a0a30a918ef6>
2012-11-06 18:41:29 +0000

    Updated to version 5.0.6

--- melting-java/src/melting/configuration/OptionManagement.java
+++ melting-java/src/melting/configuration/OptionManagement.java
@@ -254,7 +254,7 @@ public class OptionManagement {
 	/**
 	 * String version : current Melting version number.
 	 */
-	private static final String version = "5.0.5";
+	private static final String version = "5.0.6";
 	
 	/**
 	 * String dataPathwayValue : default Melting data file pathway. (files containing
