731ad7c7f22ee2b0c9f785da26569309a97b5222
niko-rodrigue <niko-rodrigue@390dfc27-8b09-476f-b734-a0a30a918ef6>
2016-09-30 11:46:21 +0000

    finished the panel on thermodynamic parameters and methods

--- trunk/melting-java/src/meltinggui/dialogs/ThermodynamicOptionPanel.java
+++ trunk/melting-java/src/meltinggui/dialogs/ThermodynamicOptionPanel.java
@@ -71,7 +71,12 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
    * A combo box where the user can select a specific nearest neighbor model for inosine base.
    */
   MeltingComboBox nnmInosineBaseCB;
+  /**
+   * A combo box where the user can select a specific nearest neighbor model for locked nucleic acid.
+   */
+  MeltingComboBox nnmLockedNucleicAcidCB;
 
+  
   /**
    * Create the panel.
    */
@@ -95,24 +100,70 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     c.anchor = GridBagConstraints.NORTHEAST;
     c.fill = GridBagConstraints.HORIZONTAL;
 
+    // Approximative formula
     add(approximativeFormulaCB, c);
     
     c.anchor = GridBagConstraints.EAST;
 
-    // Na equivalent correction
+    // nearest neighbor model
     row++;
     c.gridy = row; // row ?
     add(nearestNeighborModelCB, c);
     
-    // DMSO correction
+    // nearest neighbor model for single mismatch
     row++;
     c.gridy = row; // row ?
     add(nnmSingleMismatchCB, c);
     
-    // formamide correction
+    // formamide correction for GU base pairs in RNA sequences
     row++;
     c.gridy = row; // row ?
     add(nnmGuBasedPairInRnaCB, c);
+
+    // nearest neighbor model for tandem mismatches
+    row++;
+    c.gridy = row; // row ?
+    add(nnmTandemMismatchCB, c);
+
+    // nearest neighbor model for internal loop
+    row++;
+    c.gridy = row; // row ?
+    add(nnmInternalLoopCB, c);
+
+    // nearest neighbor model for single dangling end
+    row++;
+    c.gridy = row; // row ?
+    add(nnmSingleDanglingEndCB, c);
+
+    // nearest neighbor model for second dangling end
+    row++;
+    c.gridy = row; // row ?
+    add(nnmSecondDanglingEndCB, c);
+
+    // nearest neighbor model for long dangling end
+    row++;
+    c.gridy = row; // row ?
+    add(nnmLongDanglingEndCB, c);
+
+    // nearest neighbor model for single bulge loop
+    row++;
+    c.gridy = row; // row ?
+    add(nnmSingleBulgeLoopCB, c);
+
+    // nearest neighbor model for long bulge loop
+    row++;
+    c.gridy = row; // row ?
+    add(nnmLongBulgeLoopCB, c);
+
+    // nearest neighbor model for inosine base
+    row++;
+    c.gridy = row; // row ?
+    add(nnmInosineBaseCB, c);
+
+    // nearest neighbor model for DNA sequences containing locked nucleic acid (Al, Tl, Cl or Gl)
+    row++;
+    c.gridy = row; // row ?
+    add(nnmLockedNucleicAcidCB, c);
 
     
     // empty JPanel to fill in the empty space
@@ -142,7 +193,7 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     // approximative formula
     int index = 0;
     options = new ComboBoxOption[12];
-    options[index] = new ComboBoxOption("DNA:", "");
+    options[index] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[++index] = new ComboBoxOption("  Von Ahsen et al. 2001", "-am ahs01");
     options[++index] = new ComboBoxOption("  Marmur 1962, Chester et al. 1993", "-am che93");
     options[++index] = new ComboBoxOption("  Von Ahsen et al. 2001, Marmur 1962, Chester et al. 1993", "-am che93corr");
@@ -151,7 +202,7 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     options[++index] = new ComboBoxOption("  Santalucia et al. 1998", "-am san98");
     options[++index] = new ComboBoxOption("  Wetmur 1991 (default)", "-am wetdna91");
 
-    options[++index] = new ComboBoxOption("RNA:", "");
+    options[++index] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[++index] = new ComboBoxOption("  Wetmur 1991", "-am wetrna91");
 
     options[++index] = new ComboBoxOption("<html><u>DNA/RNA:</u>", "");
@@ -160,12 +211,14 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     approximativeFormulaCB = new MeltingComboBox(options);   
     approximativeFormulaCB.setMaximumRowCount(20);
     approximativeFormulaCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "approximative formula"));
-    approximativeFormulaCB.setToolTipText("Select a specific approximative formula."); 
+    approximativeFormulaCB.setToolTipText("<html>Select a specific approximative formula.<br>"
+        + "The approximative mode is used for oligonucleotides longer than 60 bases (the default threshold value), "
+        + "<br>otherwise the nearest neighbor model is used."); 
 
     // nearest neighbor model
     index = 0;
     options = new ComboBoxOption[14];
-    options[index] = new ComboBoxOption("DNA:", "");
+    options[index] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[++index] = new ComboBoxOption("  Allawi and Santalucia 1997 (default)", "-nn all97");
     options[++index] = new ComboBoxOption("  Breslauer et al. 1986", "-nn bre86");
     options[++index] = new ComboBoxOption("  Hicks and Santalucia 2004", "-nn san04");
@@ -173,7 +226,7 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     options[++index] = new ComboBoxOption("  Sugimoto et al. 1996", "-nn sug96");
     options[++index] = new ComboBoxOption("  Tanaka et al. 2004", "-nn tan04");
 
-    options[++index] = new ComboBoxOption("RNA:", "");
+    options[++index] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[++index] = new ComboBoxOption("  Wetmur 1991", "-nn wetrna91");
     options[++index] = new ComboBoxOption("  Xia et al. 1998 (default)", "-nn fre86");
 
@@ -185,25 +238,27 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
 
     nearestNeighborModelCB = new MeltingComboBox(options);   
     nearestNeighborModelCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "nearest neighbor model"));
-    nearestNeighborModelCB.setToolTipText("Forces to use a specific nearest neighbor model.");
+    nearestNeighborModelCB.setToolTipText("<html>Select a specific nearest neighbor model.<br>"
+        + "The approximative mode is used for oligonucleotides longer than 60 bases (the default threshold value), "
+        + "<br>otherwise the nearest neighbor model is used."); 
     
     // nearest neighbor model for single mismatch(es) in the sequences
     index = 0;
     options = new ComboBoxOption[8];
-    options[index] = new ComboBoxOption("DNA:", "");
+    options[index] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[++index] = new ComboBoxOption("  Allawi, Santalucia and Peyret 1997, 1998 and 1999", "-sinMM allsanpey");
 
     options[++index] = new ComboBoxOption("<html><u>DNA/RNA:</u>", "");
     options[++index] = new ComboBoxOption("  Watkins et al. 2011", "-sinMM wat10");
 
-    options[++index] = new ComboBoxOption("RNA:", "");
+    options[++index] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[++index] = new ComboBoxOption("  Lu et al. 2006", "-sinMM tur06");
     options[++index] = new ComboBoxOption("  Davis et al. 2007 (default)", "-sinMM zno07");
     options[++index] = new ComboBoxOption("  Davis et al. 2008", "-sinMM zno08");
 
     nnmSingleMismatchCB = new MeltingComboBox(options);   
     nnmSingleMismatchCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for single mismatch(es)"));
-    nnmSingleMismatchCB.setToolTipText("Forces to use a specific nearest neighbor model for single mismatch(es) in the sequences.");
+    nnmSingleMismatchCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for single mismatch(es) in the sequences.");
     
     // nearest neighbor model for GU base pairs in RNA sequences
     options = new ComboBoxOption[2];
@@ -212,113 +267,111 @@ public class ThermodynamicOptionPanel extends JPanel implements ActionListener, 
     
     nnmGuBasedPairInRnaCB = new MeltingComboBox(options);
     nnmGuBasedPairInRnaCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for GU base pairs in RNA"));
-    nnmGuBasedPairInRnaCB.setToolTipText("Forces to use a specific formamide correction.");
+    nnmGuBasedPairInRnaCB.setToolTipText("Select a specific formamide correction.");
 
     // nearest neighbor model for tandem mismatches
     options = new ComboBoxOption[4];
-    options[0] = new ComboBoxOption("DNA:", "");
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[1] = new ComboBoxOption("  Allawi, Santalucia and Peyret 1997, 1998 and 1999", "-tanMM allsanpey");
-    options[2] = new ComboBoxOption("RNA:", "");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[3] = new ComboBoxOption("  Mathews et al. 1999", "-tanMM tur99");
     
     nnmTandemMismatchCB = new MeltingComboBox(options);
     nnmTandemMismatchCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for tandem mismatches"));
-    nnmTandemMismatchCB.setToolTipText("Forces to use a specific nearest neighbor model for tandem mismatches in the sequences.");
+    nnmTandemMismatchCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for tandem mismatches in the sequences.");
 
     // nearest neighbor model for internal loop
     options = new ComboBoxOption[5];
-    options[0] = new ComboBoxOption("DNA:", "");
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[1] = new ComboBoxOption("  Hicks and Santalucia 2004", "-intLP allsanpey");
-    options[2] = new ComboBoxOption("RNA:", "");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[3] = new ComboBoxOption("  Lu et al. 2006 (default)", "-intLP tur06");
     options[4] = new ComboBoxOption("  Badhwarr et al. 2007, only for 1x2 loop", "-intLP zno07");
     
     nnmInternalLoopCB = new MeltingComboBox(options);
     nnmInternalLoopCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for internal loop"));
-    nnmInternalLoopCB.setToolTipText("Forces to use a specific nearest neighbor model for internal loop in the sequences.");
+    nnmInternalLoopCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for internal loop in the sequences.");
 
     // nearest neighbor model for single dangling end
     options = new ComboBoxOption[6];
-    options[0] = new ComboBoxOption("DNA:", "");
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[1] = new ComboBoxOption("  Bommarito et al. 2000 (default)", "-sinDE bom00");
     options[2] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-sinDE sugdna02");
-    options[3] = new ComboBoxOption("RNA:", "");
+    options[3] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[4] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-sinDE sugrna02");
     options[5] = new ComboBoxOption("  Miller et al. 2008 (default)", "-sinDE ser08");
     
     nnmSingleDanglingEndCB = new MeltingComboBox(options);
     nnmSingleDanglingEndCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for single dangling end"));
-    nnmSingleDanglingEndCB.setToolTipText("Forces to use a specific nearest neighbor model for single dangling end(s) in the sequences.");
+    nnmSingleDanglingEndCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for single dangling end(s) in the sequences.");
 
     // nearest neighbor model for second dangling end
     options = new ComboBoxOption[6];
-    options[0] = new ComboBoxOption("DNA:", "");
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
     options[1] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-secDE sugdna02");
-    options[2] = new ComboBoxOption("RNA:", "");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
     options[3] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-secDE sugrna02");
     options[4] = new ComboBoxOption("  O'toole et al. 2005", "-secDE ser05");
     options[5] = new ComboBoxOption("  O'toole et al. 2006 (default)", "-secDE ser06");
     
-    nnmSingleDanglingEndCB = new MeltingComboBox(options);
-    nnmSingleDanglingEndCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for second dangling end"));
-    nnmSingleDanglingEndCB.setToolTipText("Forces to use a specific nearest neighbor model for second dangling end(s) in the sequences.");
+    nnmSecondDanglingEndCB = new MeltingComboBox(options);
+    nnmSecondDanglingEndCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for second dangling end"));
+    nnmSecondDanglingEndCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for second dangling end(s) in the sequences.");
 
-    /*
-       // TODO - other combo boxes !!
-x     nearest neighbor model for  tandem mismatches
-x     nearest neighbor model for internal loop
-x     nearest neighbor model for single dangling end
-x     neighbor model for second dangling end
-     neighbor model for long dangling end
-     nearest neighbor model for single bulge loop
-     neighbor model for long bulge loop
-     nearest neighbor model for inosine base
-     nearest neighbor model for DNA sequences containing locked nucleic acid (Al, Tl, Cl or Gl)
--lonDE [name]
-    Forces to use a specific nearest neighbor model for long dangling end(s) in the sequences (self complementary sequences). You can use one of the following :
-        (DNA)
-            sugdna02 (from Ohmichi et al. 2002, only for polyA dangling ends)  (by default)
-        (RNA)
-            sugrna02 (from Ohmichi et al. 2002, only for polyA dangling ends)  (by default)
+    // nearest neighbor model for long dangling end
+    options = new ComboBoxOption[4];
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
+    options[1] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-lonDE sugdna02");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
+    options[3] = new ComboBoxOption("  Ohmichi et al. 2002, only for polyA dangling ends", "-lonDE sugrna02");
+    
+    nnmLongDanglingEndCB = new MeltingComboBox(options);
+    nnmLongDanglingEndCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for long dangling end"));
+    nnmLongDanglingEndCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for long dangling end(s) in the sequences (self complementary sequences).");
 
-    To change the file containing the thermodynamic parameters for long dangling end computation, the same syntax as the one for the -nn option is used. 
+    // nearest neighbor model for single bulge loop
+    options = new ComboBoxOption[6];
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
+    options[1] = new ComboBoxOption("  Hicks and Santalucia 2004", "-sinBU san04");
+    options[2] = new ComboBoxOption("  Tanaka et al. 2004 (default)", "-sinBU tan04");
+    options[3] = new ComboBoxOption("<html><u>RNA:</u>", "");
+    options[4] = new ComboBoxOption("  Blose et al. 2007", "-sinBU ser07");
+    options[5] = new ComboBoxOption("  Lu et al. 1999 and 2006 (default)", "-sinBU tur06");
+    
+    nnmSingleBulgeLoopCB = new MeltingComboBox(options);
+    nnmSingleBulgeLoopCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for single bulge loop"));
+    nnmSingleBulgeLoopCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for single bulge loop(s) in the sequences.");
 
--sinBU [name]
-    Forces to use a specific nearest neighbor model for single bulge loop(s) in the sequences. You can use one of the following :
-        (DNA)
-            san04 (from Hicks and Santalucia 2004)
-            tan04 (from Tanaka et al. 2004)  (by default)
-        (RNA)
-            ser07 (from Blose et al. 2007)
-            tur06 (from Lu et al. 1999 and 2006)  (by default)
+    // nearest neighbor model for long bulge loop
+    options = new ComboBoxOption[4];
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
+    options[1] = new ComboBoxOption("  Hicks and Santalucia 2004", "-lonBU san04");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
+    options[3] = new ComboBoxOption("  Mathews et al. 1999 and Lu et al 2006", "-lonBU tur06");
+    
+    nnmLongBulgeLoopCB = new MeltingComboBox(options);
+    nnmLongBulgeLoopCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for long bulge loop"));
+    nnmLongBulgeLoopCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for long bulge loop(s) in the sequences.");
 
-    To change the file containing the thermodynamic parameters for single bulge loop computation, the same syntax as the one for the -nn option is used. 
+    // nearest neighbor model for inosine base
+    options = new ComboBoxOption[4];
+    options[0] = new ComboBoxOption("<html><u>DNA:</u>", "");
+    options[1] = new ComboBoxOption("  Watkins and Santalucia 2005", "-ino san05");
+    options[2] = new ComboBoxOption("<html><u>RNA:</u>", "");
+    options[3] = new ComboBoxOption("  Wright et al. 2007", "-ino zno07");
+    
+    nnmInosineBaseCB = new MeltingComboBox(options);
+    nnmInosineBaseCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for inosine base"));
+    nnmInosineBaseCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for inosine base (I) in the sequences.");
 
--lonBU [name]
-    Forces to use a specific nearest neighbor model for long bulge loop(s) in the sequences. You can use one of the following :
-        (DNA) 
-            san04 (from Hicks and Santalucia 2004)  (by default)
-        (RNA)
-            tur06 (from Mathews et al. 1999 and Lu et al 2006)  (by default)
-            
-    To change the file containing the thermodynamic parameters for long bulge loop computation, the same syntax as the one for the -nn option is used. 
-
--ino [name]
-    Forces to use a specific nearest neighbor model for inosine base (I) in the sequences. You can use one of the following :
-        (DNA)
-            san05 (from Watkins and Santalucia 2005)  (by default)
-        (RNA)
-            zno07 (from Wright et al. 2007)  (by default)
-            
-    To change the file containing the thermodynamic parameters for inosine base computation, the same syntax as the one for the -nn option is used. 
-
--lck [name]
-    Forces to use a specific nearest neighbor model for DNA sequences containing locked nucleic acid (Al, Tl, Cl or Gl). You can use mct04 (from McTigue et al. 2004) or
-    owc11 (from Owczarzy et al. 2011, by default).
-    To change the file containing the thermodynamic parameters for locked acid nucleic computation, the same syntax as the one for the -nn option is used.
-
-     */
-
+    // nearest neighbor model for locked nucleic acid
+    options = new ComboBoxOption[2];
+    options[0] = new ComboBoxOption("  McTigue et al. 2004", "-lck mct04");
+    options[1] = new ComboBoxOption("  Owczarzy et al. 2011", "-lck owc11");
+    
+    nnmLockedNucleicAcidCB = new MeltingComboBox(options);
+    nnmLockedNucleicAcidCB.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black, 1, true), "NN model for locked nucleic acid"));
+    nnmLockedNucleicAcidCB.setToolTipText("<html>Select a specific nearest neighbor model <br>for DNA sequences containing locked nucleic acid (Al, Tl, Cl or Gl).");
 
   }
   
